90c4e4b8313306b57fa291320fcc1b28e0160525
kingufl <kingdgp@ufl.edu>
2021-03-12 21:36:07 -0500

    Delete src directory

--- src/rbic.r
+++
@@ -1,8 +0,0 @@
-rm(list=ls(all=TRUE))
-library(mclust)
-data=read.csv("bic.csv",sep=" ",header=F)
-names(data)=c("k","loglik","bic","boot")
-n=22032839
-kmersize=4
-bic2=bic("EII", data$loglik, n, kmersize, data$k)
-boxplot(bic2~data$k)
